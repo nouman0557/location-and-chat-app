4555204ed40b13cd42f018d212936a03c54c9483
nouman557 <nouman.saeed@o3interfaces.com>
2021-12-10 15:16:59 +0500

    firebase changes

--- src/app/services/window.service.ts
+++ src/app/services/window.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA2clufa-g8U9QWAOJi7C52xZkNuVyaTuE",
-  authDomain: "my-project-first-330812.firebaseapp.com",
-  projectId: "my-project-first-330812",
-  storageBucket: "my-project-first-330812.appspot.com",
-  messagingSenderId: "397327658572",
-  appId: "1:397327658572:web:262bc72d51ce68cf660ee9",
-  measurementId: "G-0TRK4054R4"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA2clufa-g8U9QWAOJi7C52xZkNuVyaTuE",
+//   authDomain: "my-project-first-330812.firebaseapp.com",
+//   projectId: "my-project-first-330812",
+//   storageBucket: "my-project-first-330812.appspot.com",
+//   messagingSenderId: "397327658572",
+//   appId: "1:397327658572:web:262bc72d51ce68cf660ee9",
+//   measurementId: "G-0TRK4054R4"
+// };
 
 // initializeApp.initializeApp(firebaseConfig);
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 @Injectable()
 export class WindowService {
